547cc9d3ecf89575549442f4980cfac7a49bd61c
VitordevJava <vitordev.java@gmail.com>
2024-07-31 21:49:31 -0300

    Adicionando outro exercicio em classe diferente, dessa vez somando 4 notas e dividindo por 4 e assim calculando sua media final

---
+++ Modulo007JavaWeb/src/exerciciosMod007/SomaEdivisao.java
@@ -0,0 +1,20 @@
+package exerciciosMod007;
+
+public class SomaEdivisao {
+	public static void main(String[] args) {
+		
+		double N1 = 98;
+		double N2 = 78;
+		double N3 = 39;
+		double N4 = 67;
+		
+		double MediaFinal = (N1 + N2 + N3 + N4)/4;
+		System.out.println("A soma de todas as notas são :"+(N1 + N2 + N3 + N4));
+		
+		
+		System.out.println("Já sua media final é :" + MediaFinal);
+		
+		
+	}
+
+}
