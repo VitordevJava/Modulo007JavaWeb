8d005a62c4e3c9b73b44513ee545ba5319efc95a
VitordevJava <vitordev.java@gmail.com>
2024-07-31 21:31:14 -0300

    Exercicio de soma de 4 numeros em 4 variaveis

---
+++ Modulo007JavaWeb/src/exerciciosMod007/SomaNumber.java
@@ -0,0 +1,14 @@
+package exerciciosMod007;
+
+public class SomaNumber {
+	public static void main(String[] args) {
+		int Nota1 = 90;
+		int Nota2 = 55;
+		int Nota3 = 77;
+		int Nota4 = 28;		
+	System.out.println("A soma de todos os numeros são :"+(Nota1 + Nota2 + Nota3 + Nota4)/4);
+		
+	}
+	
+
+}
