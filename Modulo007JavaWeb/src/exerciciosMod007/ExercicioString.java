b8031505d8025f225d1efe297ade8c8d89de8be8
VitordevJava <vitordev.java@gmail.com>
2024-07-31 22:37:06 -0300

    Nova atualização, inserindo dados para serem impressos na tela na variavel do tipo string.

--- Modulo007JavaWeb/src/exerciciosMod007/ExercicioString.java
+++ Modulo007JavaWeb/src/exerciciosMod007/ExercicioString.java
@@ -4,10 +4,14 @@ public class ExercicioString {
 	public static void main(String[] args) {
 		String Pessoa;
 	
-		String cpf;
-		String NomeCompl;
-		String Endereço;
-		String Numero;
+		String cpf = "70861133344";
+		String NomeCompl = "Vitor Carvalho";
+		String Endereço = "Vila portuguesa";
+		String Numero = "61991220000";
+		
+		System.out.println("Meu Nome é :\n" + NomeCompl + "\nMeu Cpf é ;\n" + cpf
+			+	"\nMoro no endereço\n" + Endereço + "\nMeu numero de telefone é\n" + Numero);
 	}
+	
 
 }
