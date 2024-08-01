c594679609c021f7671df259c6ff7f1efdf198e8
VitordevJava <vitordev.java@gmail.com>
2024-07-31 22:18:18 -0300

    Exercitando e aprendendo sobre a variavel do tipo string.

---
+++ Modulo007JavaWeb/src/exerciciosMod007/ExercicioString.java
@@ -0,0 +1,13 @@
+package exerciciosMod007;
+
+public class ExercicioString {
+	public static void main(String[] args) {
+		String Pessoa;
+	
+		String cpf;
+		String NomeCompl;
+		String Endereço;
+		String Numero;
+	}
+
+}
